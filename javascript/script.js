add02a64265c3a8e17f0b10a685e3fa68857aa4e
mahfoudi <mohamed.elmahfoudi@outlook.com>
2023-08-15 17:44:29 +0100

    add  js scroll

--- javascript/script.js
+++ javascript/script.js
@@ -10,3 +10,16 @@ function hundelMenu(){
   menu.classList.toggle("is-active");
   actions.classList.toggle("is-active");
 }
+
+let scroll = document.getElementById('scroll');
+
+window.onscroll = function () {
+  if (scrollY >= 260){
+    scroll.style.display = 'block';
+  }else{
+    scroll.style.display = 'none';
+  } 
+}
+
+
+
